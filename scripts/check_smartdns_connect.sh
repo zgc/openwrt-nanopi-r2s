5dc21cd6d715945e7d557ecf15ea458a178bc0a9
Guocheng Zheng <121431418@qq.com>
2021-03-17 16:23:46 +0800

    updated

--- scripts/check_smartdns_connect.sh
+++ scripts/check_smartdns_connect.sh
@@ -4,7 +4,7 @@ tries=0
 LOG=/tmp/check_smartdns_connect.log
 ALIYUN=alidns.com
 while [ $tries -lt 5 ]; do
-  NSLOOKUP=$(nslookup $ALIYUN | grep -v grep | grep 'Address ' | wc -l)
+  NSLOOKUP=$(nslookup $ALIYUN 127.0.0.1#6053 2>/dev/null | grep -v grep | grep 'Address ' | wc -l)
   if [ ${NSLOOKUP} -ne 0 ]; then
     echo $DATE check smartdns connect: OK >>$LOG
     exit 0
--- scripts/check_wan_connect.sh
+++ scripts/check_wan_connect.sh
@@ -2,11 +2,11 @@
 DATE=$(date +%Y-%m-%d-%H:%M:%S)
 tries=0
 LOG=/tmp/check_wan_connect.log
-PPPOE=$(/sbin/ifconfig | grep -v grep | grep 'pppoe-wan' | wc -l)
+PPPOE=$(/sbin/ifconfig 2>/dev/null | grep -v grep | grep 'pppoe-wan' | wc -l)
 ALIYUN=223.5.5.5
 if [ ${PPPOE} -ne 0 ]; then
   while [ $tries -lt 5 ]; do
-    PING=$(ping -c1 $ALIYUN | grep -v grep | grep '64 bytes' | wc -l)
+    PING=$(ping -c1 $ALIYUN 2>/dev/null | grep -v grep | grep '64 bytes' | wc -l)
     if [ ${PING} -ne 0 ]; then
       echo $DATE check ppp connect: OK >>$LOG
       exit 0
@@ -19,10 +19,10 @@ if [ ${PPPOE} -ne 0 ]; then
   done
 else
   while [ $tries -lt 5 ]; do
-    eth1=$(/sbin/ifconfig eth1 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+    eth1=$(/sbin/ifconfig eth1 2>/dev/null | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
     if [ -n ${eth1} ]; then
       GATEWAY=$(echo $eth1 | awk 'BEGIN{FS=OFS="."}{$NF=1;print}')
-      PING=$(ping -c1 $GATEWAY | grep -v grep | grep '64 bytes' | wc -l)
+      PING=$(ping -c1 $GATEWAY 2>/dev/null | grep -v grep | grep '64 bytes' | wc -l)
       if [ ${PING} -ne 0 ]; then
         echo $DATE check wan connect: OK >>$LOG
         exit 0
