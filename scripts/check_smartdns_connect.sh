431daf25cec36e6160a7f41a8d2fbf1c0608c4bd
Guocheng Zheng <121431418@qq.com>
2021-07-04 20:36:55 +0800

    updated

--- scripts/check_smartdns_connect.sh
+++ scripts/check_smartdns_connect.sh
@@ -3,19 +3,28 @@ DATE=$(date +%Y-%m-%d-%H:%M:%S)
 tries=0
 LOG=/tmp/check_smartdns_connect.log
 ALIYUN=alidns.com
+DNSPOD=dnspod.cn
 SMARTDNS_PORT=$(cat /etc/config/smartdns | grep "option port" | cut -d: -f2 | awk '{ print $3}' | cut -d "'" -f 2)
 SMARTDNS_ENABLED=$(cat /etc/config/smartdns | grep -m 1 "option enabled" | cut -d: -f2 | awk '{ print $3}' | cut -d "'" -f 2)
 if [ ${SMARTDNS_ENABLED} -eq 1 ]; then
   while [ $tries -lt 5 ]; do
+    echo $DATE check $ALIYUN start >>$LOG
     NSLOOKUP=$(nslookup $ALIYUN 127.0.0.1#${SMARTDNS_PORT} 2>/dev/null | grep -v grep | grep 'Address ' | wc -l)
     if [ ${NSLOOKUP} -ne 0 ]; then
       echo $DATE check smartdns connect: OK >>$LOG
       exit 0
     else
-      let tries++
-      echo $DATE tries: $tries, smartdns restart >>$LOG
-      /etc/init.d/smartdns restart
-      sleep 10
+      echo $DATE check $DNSPOD start >>$LOG
+      NSLOOKUP=$(nslookup $DNSPOD 127.0.0.1#${SMARTDNS_PORT} 2>/dev/null | grep -v grep | grep 'Address ' | wc -l)
+      if [ ${NSLOOKUP} -ne 0 ]; then
+        echo $DATE check smartdns connect: OK >>$LOG
+        exit 0
+      else
+        let tries++
+        echo $DATE tries: $tries, smartdns restart >>$LOG
+        /etc/init.d/smartdns restart
+        sleep 10
+      fi
     fi
   done
 else
--- scripts/check_wan_connect.sh
+++ scripts/check_wan_connect.sh
@@ -4,17 +4,26 @@ tries=0
 LOG=/tmp/check_wan_connect.log
 PPPOE=$(/sbin/ifconfig 2>/dev/null | grep -v grep | grep 'pppoe-wan' | wc -l)
 ALIYUN=223.5.5.5
+DNSPOD=119.29.29.29
 if [ ${PPPOE} -ne 0 ]; then
   while [ $tries -lt 5 ]; do
+    echo $DATE check $ALIYUN start >>$LOG
     PING=$(ping -c1 $ALIYUN 2>/dev/null | grep -v grep | grep '64 bytes' | wc -l)
     if [ ${PING} -ne 0 ]; then
       echo $DATE check ppp connect: OK >>$LOG
       exit 0
     else
-      let tries++
-      echo $DATE tries: $tries, ppp restart >>$LOG
-      ifdown wan | ifup wan
-      sleep 10
+      echo $DATE check $DNSPOD start >>$LOG
+      PING=$(ping -c1 $DNSPOD 2>/dev/null | grep -v grep | grep '64 bytes' | wc -l)
+      if [ ${PING} -ne 0 ]; then
+        echo $DATE check ppp connect: OK >>$LOG
+        exit 0
+      else
+        let tries++
+        echo $DATE tries: $tries, ppp restart >>$LOG
+        ifdown wan | ifup wan
+        sleep 10
+      fi
     fi
   done
 else
